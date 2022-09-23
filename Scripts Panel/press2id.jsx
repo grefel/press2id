d4241d7188339d8e8ce240321b90e538b0d4c770
Gregor Fellenz <gregor.fellenz@publishingx.de>
2022-09-23 09:30:33 +0200

    typo

--- Scripts Panel/press2id.jsx
+++ Scripts Panel/press2id.jsx
@@ -415,7 +415,7 @@ function processDok(dok) {
                     }
                     catch (e) {
                         if (e.number == 79110) {
-                            log.info("Hyperlink Name alreadey used");
+                            log.info("Hyperlink Name already used");
                         }
                         else {
                             log.warn(e);
